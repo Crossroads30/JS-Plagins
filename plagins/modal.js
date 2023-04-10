8645b7a82b44e3f3da11902a7af824c36ef70e36
Crossroads30 <crossroads30@mail.ru>
2023-04-10 14:23:56 +0300

    feat: add functionality for modal

--- plagins/modal.js
+++ plagins/modal.js
@@ -1,51 +1,95 @@
+Element.prototype.appendAfter = function (element) {// метод для реализации футера в модалку
+  element.parentNode.insertBefore(this/* - это Элемент(в данном случае футер)*/, element.nextSibling)
+}
+
+function _createModalFooter(buttons = []) {// создания футера
+  if (buttons.length === 0) {
+    return document.createElement('div')
+  }
+
+  const wrapper = document.createElement('div')
+  wrapper.classList.add('modal__footer')
+
+
+  return wrapper
+}
+
 function _createModal(options) {
-   const modal = document.createElement('div')
-   modal.classList.add('vmodal')
-   modal.insertAdjacentHTML('afterbegin', `
-      <div class="modal__overlay">
-         <div class="modal__wrapper">
-            <div class="modal__close-btn"><span></span></div>
-            <div class="modal__window">
-               <img src="assets/pets-jennifer.png" class="modal__image">
-               <div class="modal__content pet">
-                  <h2 class="pet__title">
-                     Jennifer
-                     <p class="pet__subtite">Dog - Labrador</p>
-                  </h2>
-                  <p class="pet__info">Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.</p>
-                  <ul class="pet__parameters">
-                     <li>Age: 2 months</li>
-                     <li>Inoculations: none</li>
-                     <li>Diseases: none</li>
-                     <li>Parasites: none</li>
-                  </ul>
-               </div>
+  const DEFAULT_WIDTH = '952px'
+  const modal = document.createElement('div')
+  modal.classList.add('vmodal')
+  modal.insertAdjacentHTML('afterbegin', `
+        <div class="modal__overlay" data-close="true">
+          <div class="modal__wrapper" style="width: ${options.width || DEFAULT_WIDTH}">
+            ${options.closable ? `<div class="modal__close-btn" data-close="true"><span data-close="true">&#215</span></div>` : ''}
+              <div class="modal__window" data-footer>
+              ${options.image}
+              <div class="modal__content pet" data-content>
+              <h2 class="pet__title">
+                 ${options.name || "Pets'name"}
+                 <p class="pet__subtite">${options.type || "Pets'type"}-${options.breed}</p>
+              </h2>
+              <p class="pet__info">${options.description || "Pets'info"}</p>
+              <ul class="pet__parameters">
+                 <li>Age: ${options.age || "Pets'age"}</li>
+                 <li>Inoculations: ${options.inoculations || "Pets'inoculations"}</li>
+                 <li>Diseases: ${options.diseases || "Pets'diseases"}</li>
+                 <li>Parasites: ${options.parasites || "Pets'parasites"}</li>
+              </ul>
+           </div>
+              </div>
             </div>
-         </div>
-      </div>
+          </div>
    `)
-   document.body.appendChild(modal)
-   return modal
+  const footer = _createModalFooter(options.footerButtons)
+  footer.appendAfter(modal.querySelector('[data-footer]'))// вставляем футер после элемента с этим дата атрибутом
+  document.body.appendChild(modal)//добавление узла в дом дерева
+  return modal
 }
 
 $.modal = function (options) {
-   const ANIMATION_SPEED = 200
-   const $modal = _createModal(options)
-   let closing = false
+  const ANIMATION_SPEED = 200
+  const $modal = _createModal(options)
+  let closing = false
+  let destroyed = false
 
-   return {
-      open() {
-         !closing && $modal.classList.add('open')
-      },
-      close() {
-         closing = true
-         $modal.classList.remove('open')
-         $modal.classList.add('hide')
-         setTimeout(() => {
-            $modal.classList.remove('hide')
-            closing = false
-         }, ANIMATION_SPEED)
-      },
-      destroy() { }
-   }
+  const modal = {
+    open() {
+      if (destroyed) {
+        return console.log('Modal is destroyed')
+      }
+      !closing && $modal.classList.add('open')
+    },
+    close() {
+      closing = true
+      $modal.classList.remove('open')
+      $modal.classList.add('hide')
+      setTimeout(() => {
+        $modal.classList.remove('hide')
+        closing = false
+      }, ANIMATION_SPEED)
+    },
+  }
+
+  const listener = event => {// функция слушателя клика на закрытие окна
+    //console.log('Clicked', event.target.dataset.close)
+    if (event.target.dataset.close) {// клик на элементы с дата-атрибутами
+      modal.close()
+    }
+  }
+
+  $modal.addEventListener('click', listener)// обработчик клика
+
+  return Object.assign(modal, {
+    destroy() {
+      $modal.parentNode.removeChild($modal)// удаления узла из дом дерева
+      $modal.removeEventListener('click', listener)// удаление обработчика 
+      destroyed = true
+    },
+    setContent(html) {
+      $modal.querySelector('[data-content]').innerHTML = html//можно изменять контент в элементах с этим дата атрибутом
+    }
+  })
 }
+
+
